9ac72568acf5caea471601f996b2446ba2a1d981
aidanabdikarim <aidana.abdikarim@nu.edu.kz>
2026-02-08 16:49:28 +0500

    Initial Commit

---
+++ final/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  final
+//
+//  Created by Aidana Abdikarim on 08.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ finalTests/finalTests.swift
@@ -0,0 +1,16 @@
+//
+//  finalTests.swift
+//  finalTests
+//
+//  Created by Aidana Abdikarim on 08.02.2026.
+//
+
+import Testing
+
+struct finalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
